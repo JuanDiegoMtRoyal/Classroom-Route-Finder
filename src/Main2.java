ce50fa7e49d69f32e2596d482260e6dc8dcfc57e
Kini Chea <112512178+kchea186@users.noreply.github.com>
2025-03-03 00:19:38 -0700

    Second main file to avoid bombing the first

---
+++ src/Main2.java
@@ -0,0 +1,17 @@
+public class Main2 {
+    public static void main(String[] args) {
+        // Create the graph
+        Graph graph = new Graph();
+
+        // Parse the building map
+        Parser parser = new Parser(graph);
+        parser.initializeBuildingMap("bissetFloor1.txt");
+
+        // Create the Classroom Route Finder
+        CRF crf = new CRF(graph);
+
+        // Start user input handler
+        UserInput userInput = new UserInput(crf);
+        userInput.start();
+    }
+}
