642bc530c2abd5f506d735cc1d1f7a9084d457c6
kchea186 <kchea186@mtroyal.ca>
2025-03-23 19:57:36 -0600

    Added display function in node class

--- haskell/Node.hs
+++ haskell/Node.hs
@@ -2,18 +2,21 @@ module Node
 (
     Node,
 
-    displayInfo,
-    equals
+    equals,
+    displayInfo
 )
 where
 
 data Node = Node
-    { name :: String,
-      hallway :: Hallway,
-      positionAlongHallway :: Int,
-      floor :: Int
+    { nodeName :: String,
+      nodeHallway :: Hallway,
+      nodePositionAlongHallway :: Int,
+      nodeFloor :: Int
     } deriving (Show)
 
 -- compares the name of 2 nodes => returns true if the same, else false
 equals :: Node -> Node -> Bool
 equals (Node name1 _ _ _) (Node name2 _ _ _) = name1 == name2
+
+displayInfo :: Node -> IO()
+displayInfo node = putStrLn ("Node Info:\nName: " ++ (nodeName node) ++ " Hallway: " ++ (nodeHallway node) ++ " nodePositionAlongHallway node) ++ " Floor: " ++ (nodeFloor node))
