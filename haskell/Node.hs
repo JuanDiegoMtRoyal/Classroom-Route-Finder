eeddb8a6c1145d91508549ef7f2a40690e8d5c25
kchea186 <kchea186@mtroyal.ca>
2025-03-27 10:07:22 -0600

    added constructor and matched current java class

--- haskell/Node.hs
+++ haskell/Node.hs
@@ -11,10 +11,15 @@ import Hallway (Hallway(..))
 
 data Node = Node
     { nodeName :: String,
+      nodeIntersection :: Intersection,
       nodeHallway :: Hallway,
       nodePositionAlongHallway :: Int,
       nodeFloor :: Int
     } deriving (Show)
+
+-- constructor
+createNode :: String -> Intersection -> Hallway -> Int -> Floor -> Node
+createNode name intersection hallway positionAlongHallway floor
 
 -- compares the name of 2 nodes => returns true if the same, else false
 equals :: Node -> Node -> Bool
