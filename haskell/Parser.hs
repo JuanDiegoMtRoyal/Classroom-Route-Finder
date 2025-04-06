16e1f3a54fba548ad9691e10cdd21077c4735457
kchea186 <kchea186@mtroyal.ca>
2025-04-06 09:38:59 -0600

    added helper function to parse line type

--- haskell/Parser.hs
+++ haskell/Parser.hs
@@ -1,6 +1,37 @@
 module Parser where
 
+import Data.List as List
+import Data.Maybe as Maybe
+import System.IO
+import Control.Monad
+
 import DataTypes
 import HallwayOps
 import IntersectionOps
+import Graph
+
+data Parser = Parser
+    { pGraph = Graph
+    }
+
+createParser :: Parser
+createParser = Parser emptyGraph
+
+initializeBuildingMap :: Parser -> String -> IO Graph
+initializeBuildingMap parser filename = do
+    content <- readFile filename
+    let lines = filter
+    -- FIX
+
+-- Helper function that checks the type of the Parser
+-- ie.) # Hallway Format:
+-- ie.) # Classroom Format: etc..
+getSectionType :: String -> Maybe String
+getSectionType line
+    | "# Intersection Format" `isPrefixOf` line = Just "Intersection"
+    | "# Hallway Format" `isPrefixOf` line = Just "Hallway"
+    | "# Classroom Format" `isPrefixOf` line = Just "Classroom"
+    | "# Stairs Format" `isPrefixOf` line = Just "Stairs"
+    | "# Elevator Format" `isPrefixOf` line = Just "Elevator"
+    | otherwise = Nothing
 
